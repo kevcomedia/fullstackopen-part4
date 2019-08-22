ec561ce7e5df93c2a057a15c5856d3a3430d13e7
Kev Comedia <kevcomedia@gmail.com>
2019-08-22 12:25:20 +0800

    Move blog routes to separate file

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ const app = express()
 const bodyParser = require('body-parser')
 const cors = require('cors')
 const mongoose = require('mongoose')
-const Blog = require('./models/blog')
+const blogRouter = require('./controllers/blogs')
 
 const mongoUrl = process.env.MONGODB_URI
 mongoose.connect(mongoUrl, { useNewUrlParser: true })
@@ -13,23 +13,7 @@ mongoose.connect(mongoUrl, { useNewUrlParser: true })
 app.use(cors())
 app.use(bodyParser.json())
 
-app.get('/api/blogs', (request, response) => {
-  Blog
-    .find({})
-    .then(blogs => {
-      response.json(blogs)
-    })
-})
-
-app.post('/api/blogs', (request, response) => {
-  const blog = new Blog(request.body)
-
-  blog
-    .save()
-    .then(result => {
-      response.status(201).json(result)
-    })
-})
+app.use('/api/blogs', blogRouter)
 
 const PORT = process.env.PORT
 app.listen(PORT, () => {
