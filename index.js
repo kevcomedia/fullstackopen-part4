756fc28d09d96788fe7d0cbc732e1dc10398ffc3
Kev Comedia <kevcomedia@gmail.com>
2019-08-22 12:32:50 +0800

    Move application code to app.js

--- index.js
+++ index.js
@@ -1,21 +1,8 @@
 const config = require('./utils/config')
 const http = require('http')
-const express = require('express')
-const app = express()
-const bodyParser = require('body-parser')
-const cors = require('cors')
-const mongoose = require('mongoose')
-const blogRouter = require('./controllers/blogs')
+const app = require('./app')
 
-const mongoUrl = config.MONGODB_URI
-mongoose.connect(mongoUrl, { useNewUrlParser: true })
-
-app.use(cors())
-app.use(bodyParser.json())
-
-app.use('/api/blogs', blogRouter)
-
-const PORT = config.PORT
-app.listen(PORT, () => {
-  console.log(`Server running on port ${PORT}`)
+const server = http.createServer(app)
+server.listen(config.PORT, () => {
+  console.log(`Server running on port ${config.PORT}`)
 })
