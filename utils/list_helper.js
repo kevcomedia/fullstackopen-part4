44190e21b2cc819723ba7c346fbec16590eb833c
Kev Comedia <kevcomedia@gmail.com>
2019-08-23 20:11:43 +0800

    Add favorite blog helper - 4.5

--- utils/list_helper.js
+++ utils/list_helper.js
@@ -6,7 +6,18 @@ const totalLikes = (blogs) => {
   return blogs.reduce((likes, blog) => likes + blog.likes, 0)
 }
 
+const favoriteBlog = (blogs) => {
+  return blogs.reduce((favorite, blog) => {
+    if (favorite === null || blog.likes > favorite.likes) {
+      return blog
+    } else {
+      return favorite
+    }
+  }, null)
+}
+
 module.exports = {
   dummy,
   totalLikes,
+  favoriteBlog,
 }
