3051f2f4656b0c162314bdd51fbca9b51ff3ae50
Kev Comedia <kevcomedia@gmail.com>
2019-08-23 10:53:37 +0800

    Add logger middleware

--- app.js
+++ app.js
@@ -5,12 +5,14 @@ const bodyParser = require('body-parser')
 const cors = require('cors')
 const mongoose = require('mongoose')
 const blogRouter = require('./controllers/blogs')
+const middleware = require('./utils/middleware')
 
 const mongoUrl = config.MONGODB_URI
 mongoose.connect(mongoUrl, { useNewUrlParser: true })
 
 app.use(cors())
 app.use(bodyParser.json())
+app.use(middleware.requestLogger)
 
 app.use('/api/blogs', blogRouter)
 
