95815a949c6523eb9b302f8c989091ae9e75ff2a
Kev Comedia <kevcomedia@gmail.com>
2019-08-25 20:45:31 +0800

    Set default likes value to 0 - 4.11

--- tests/blog_api.test.js
+++ tests/blog_api.test.js
@@ -47,6 +47,22 @@ test('can create blog', async () => {
   expect(titles).toContain('New blog')
 })
 
+test('missing likes property defaults to 0', async () => {
+  const newBlog = {
+    title: 'New blog',
+    author: 'John Doe',
+    url: 'http://example.com',
+  }
+
+  const result = await api.post('/api/blogs')
+    .send(newBlog)
+    .expect(201)
+    .expect('Content-Type', /application\/json/)
+
+  const createdBlog = result.body
+  expect(createdBlog.likes).toBe(0)
+})
+
 afterAll(() => {
   mongoose.connection.close()
 })
